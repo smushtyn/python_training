ea6afc1918f06ce24d84e3369a82c45301f3a9e3
smushtyn <smushtyn@gmail.com>
2015-12-06 19:49:30 +0200

    Добавлен тест удаления первой группы, фикстура вынесена в отдельный файл, браузер открыт на протяжении всей сессии прогона тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
+
 from model.group import Group
-
-
-@pytest.fixture #-------------------------------#указывает для pytest, что функция создает именно фикстуру
-                                                #(@ превращает метод/функцию в инициализатор фикстуры)
-def app(request):#------------------------------#функция инициализации фикстуры
-    fixture = Application()#--------------------#создана фикстура - обьект типа Application
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
